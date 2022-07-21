8b5fe3de76bba464a36722f990af5ea500c210e9
Altay Atalay <altayatalay@protonmail.com>
2022-07-22 01:12:41 +0300

    updated zsh and tmux configs, removed oh-my-tmux, updated script

--- sync_config_files.sh
+++ sync_config_files.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# nvim files
 rsync -vr ~/.config/nvim/ ./files/nvim/ --exclude=plugin --delete
+
+# zsh files
+rsync -v ~/.zprofile ./files/zsh/
+
+# tmux files
+rsync -v ~/.tmux.conf ./files/tmux/
