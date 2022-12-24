d08aff71f94f8c6ef9ed29470256ed572a350680
Altay Atalay <altayatalay@protonmail.com>
2022-12-24 11:24:51 +0300

    moved files/nvim to a sperate git repo

--- files/nvim/ftplugin/java.lua
+++
@@ -1,1 +0,0 @@
-require('jdtls').start_or_attach(require('user.lsp.server-settings.jdtls'))
--- files/nvim/lua/user/hop.lua
+++
@@ -1,9 +0,0 @@
--- Use a protected call
-local status_ok, hop = pcall(require, 'hop')
-if not status_ok then
-	return
-end
-
-hop.setup({
-	multi_windows = true,
-})
